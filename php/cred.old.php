30942056b79e5eccdd942368827aba7ac1c49bf4
Mateusz <32539815+mateoo1@users.noreply.github.com>
2023-06-25 01:31:14 +0200

    Create cred.old.php

---
+++ php/cred.old.php
@@ -0,0 +1,24 @@
+<?php
+
+require "parameters.php";
+
+// establish connection
+$dsn = "mysql:host=$dbHost;dbname=$dbName;charset=utf8";
+    
+try {
+    
+    $db = new PDO($dsn, $username, $password, [
+        PDO::ATTR_EMULATE_PREPARES => false, 
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+        ]);
+                
+    } catch (PDOException $error) {
+        if ($debugMode == TRUE) {
+            echo $error; 
+        } else {
+            //do not print errors
+        }
+    exit();
+    }
+?>
