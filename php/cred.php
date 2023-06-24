9e6d3aeabe01dd989306c3311c282ae2b925315d
Mateusz <32539815+mateoo1@users.noreply.github.com>
2023-06-25 01:32:32 +0200

    Update cred.php

--- php/cred.php
+++ php/cred.php
@@ -3,22 +3,19 @@
 require "parameters.php";
 
 // establish connection
-$dsn = "mysql:host=$dbHost;dbname=$dbName;charset=utf8";
-    
-try {
-    
-    $db = new PDO($dsn, $username, $password, [
-        PDO::ATTR_EMULATE_PREPARES => false, 
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-        ]);
-                
-    } catch (PDOException $error) {
-        if ($debugMode == TRUE) {
-            echo $error; 
-        } else {
-            //do not print errors
-        }
-    exit();
+$conn = new mysqli($dbHost, $username, $password, $dbName);
+
+// check connection
+if ($conn->connect_error) {
+    if ($debugMode == TRUE) {
+        echo "Connection failed: " . $conn->connect_error;
+    } else {
+        // do not print errors
     }
+    exit();
+}
+
+// set character set
+$conn->set_charset("utf8");
+
 ?>
