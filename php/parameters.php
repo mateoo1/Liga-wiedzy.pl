b8e1467d8b6fc7cd689e9006543607f947a98042
Mateusz <32539815+mateoo1@users.noreply.github.com>
2025-07-01 09:59:17 +0200

    Update parameters.php

--- php/parameters.php
+++ php/parameters.php
@@ -3,7 +3,7 @@
 // Database crdentials
 // $dbHost = "172.10.3.2";
 // $dbHost = "localhost:3306";
-$dbHost = "liga-mysql-svc:3306";
+$dbHost = "liga-mysql:3306";
 $dbName = "test_db";
 $username = "root";
 $password = "Dev321";
